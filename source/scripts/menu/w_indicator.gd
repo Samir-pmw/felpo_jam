ce62bd0631cfb701d39c31addaf5eb83d4512f9d
Papiro <samirbatistasantos@gmail.com>
2026-02-20 18:31:57 -0300

    descer camera adicionado

--- source/scripts/menu/w_indicator.gd
+++ source/scripts/menu/w_indicator.gd
@@ -6,9 +6,3 @@ func _ready():
 	tween.parallel().tween_property(self, "modulate:a", 0.5, 1.2)
 	tween.tween_property(self, "scale", Vector3(1.0, 1.0, 1.0), 1.2).set_trans(Tween.TRANS_SINE)
 	tween.parallel().tween_property(self, "modulate:a", 1.0, 1.2)
-
-func _process(_delta):
-	if Input.is_action_just_pressed("ui_up"): 
-		if owner.has_method("subir_camera"):
-			owner.subir_camera()
-		queue_free()
