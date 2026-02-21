faecde822e851f8130c8df9d3ce71051d42ddc29
AkiraTheShadow <matheusmpagel@gmail.com>
2026-02-20 22:09:43 -0300

    aleatorização do mapa

--- source/scripts/map/map.gd
+++ source/scripts/map/map.gd
@@ -2,22 +2,31 @@ extends Node2D
 
 @export var preset_botao = preload("res://scenes/map/botao_fase.tscn")
 
-@export var imagens: Array[Texture2D]
-var tipos_fases = ["Combate", "Tesouro", "Fogueira", "Loja", "Puzzle"]
-
+@export var fases: Array[FaseData] = []
+		
 func _ready():
+	fases_da_pasta()
 	randomize()
 	criar_mapa()
-	
+
+func fases_da_pasta():
+	var path = "res://resources/fases/"
+	var arquivos = DirAccess.get_files_at(path)
+	for nome_arquivo in arquivos:
+		var recurso = load(path+nome_arquivo)
+		fases.append(recurso)
+		
 func criar_mapa():
-	for marcador in $Fases.get_children():
+	var opcao_fases: Array[FaseData] = []
+	var marcador = $Fases.get_children()
+	for i in fases:
+		for j in range(i.limite):
+			opcao_fases.append(i)
+	opcao_fases.shuffle()
+	
+	for i in range(marcador.size()):
 		var novo_botao = preset_botao.instantiate()
-		var numero = randi_range(0, imagens.size() - 1)
-		novo_botao.fase(imagens[numero], tipos_fases[numero])
-		novo_botao.global_position = marcador.global_position
+		var dados_fase = opcao_fases[i]
+		novo_botao.global_position = marcador[i].global_position
+		novo_botao.fase(dados_fase)
 		add_child(novo_botao)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
